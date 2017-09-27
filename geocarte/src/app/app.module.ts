1ddd178f277e90fd60cd6dd957bd22a8381affb1
Antoine <ravet.antoine.eln@gmail.com>
2017-09-27 18:21:43 +0200

    Suppression modal

--- geocarte/src/app/app.module.ts
+++ geocarte/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { NavComponent } from './components/nav/nav.component';
-import { NgbdModalBasic } from './components/ngbd-modal-basic/ngbd-modal-basic.component';
-
 
 import { AgmCoreModule } from '@agm/core';
 import {MatSidenavModule} from '@angular/material';
@@ -17,7 +15,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     AppComponent,
     MapComponent,
     NavComponent,
-    NgbdModalBasic
   ],
   imports: [
     BrowserModule,
