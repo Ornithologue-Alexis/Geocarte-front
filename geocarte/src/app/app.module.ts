4b5cb2ef8e35758e10cbe7a112c6deda1a0c2872
Antoine <ravet.antoine.eln@gmail.com>
2017-09-27 16:32:10 +0200

    Ajout component modal et création dossier component

--- geocarte/src/app/app.module.ts
+++ geocarte/src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
+import { MapComponent } from './components/map/map.component';
 
 
 import { AgmCoreModule } from '@agm/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from './components/modal/modal.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCAZKYezUJLxkFdkaH1pxNqJeAX8OV5Ej0	'
     })
