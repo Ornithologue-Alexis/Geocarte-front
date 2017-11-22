2f9056ab1da228d79f95481518bde6a5a19f15a8
Alexis Renault <renaultalexis35@gmail.com>
2017-11-22 18:04:59 +0100

    Nav component improvements

--- geocarte/src/app/components/nav/nav.component.ts
+++ geocarte/src/app/components/nav/nav.component.ts
@@ -7,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  myCards = false;
+  profil = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openMenuPage(page:string)
+  {
+    if(page === "myCards" && !this.myCards)
+    {
+      this.myCards = true;
+      this.profil = false;
+    }else if(page === "profil" && !this.profil){
+      this.myCards = false;
+      this.profil = true;
+    }else{
+      this.myCards = false;
+      this.profil = false;
+    }
   }
 
 }
