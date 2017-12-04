b372a3d0bd547547c2dfd0ed809d57fc095fc0c7
Antoine Ravet <ravet.antoine.eln@gmail.com>
2017-12-04 10:40:24 +0100

    Ajout de la gestion des marqueurs

--- geocarte/src/app/components/map/map.component.ts
+++ geocarte/src/app/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MdDialog, MdDialogRef, MD_DIALOG_DATA} from '@angular/material';
+declare let google: any;
 
 
 @Component({
@@ -9,8 +10,17 @@ import {MdDialog, MdDialogRef, MD_DIALOG_DATA} from '@angular/material';
 })
 export class MapComponent implements OnInit {
 
-  lat: number = 48.117266 ;
-  lng: number = -1.6777925999999752;
+  lat: number = 48.1246539 ;
+  lng: number = -1.652399100000025;
+
+  markers: marker[] = [
+    {
+      lat: 48.1246539,
+      lng: -1.652399100000025,
+      draggable: false,
+      icon: 'red'
+    },
+  ];
 
   constructor(public dialog: MdDialog) {
   }
@@ -25,8 +35,40 @@ export class MapComponent implements OnInit {
       height: '75%'
     });
   }
-  mapClicked($event: MouseEvent) {
-    console.log("ici");
+  mapDoubleClicked($event) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      icon: 'blue'
+    });
+    let dialogRef = this.dialog.open(CardTemplate, {
+      panelClass: 'myapp-no-padding-dialog',
+      width: '75%',
+      height: '75%'
+    });
+    this.getGeoLocation($event.coords.lat, $event.coords.lng);
+  }
+
+
+  getGeoLocation(lat: number, lng: number) {
+    if (navigator.geolocation) {
+      let geocoder = new google.maps.Geocoder();
+      let latlng = new google.maps.LatLng(lat, lng);
+      let request = { latLng: latlng };
+
+      geocoder.geocode(request, (results, status) => {
+        if (status == google.maps.GeocoderStatus.OK) {
+          let result = results[0];
+          let rsltAdrComponent = result.address_components;
+          let resultLength = rsltAdrComponent.length;
+          if (result != null) {
+            return result.formatted_address;
+          } else {
+            alert("No address available!");
+          }
+        }
+      });
+    }
   }
 }
 
---
+++ geocarte/src/app/interfaces/marker.ts
@@ -0,0 +1,7 @@
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  draggable?: boolean;
+  icon?: string;
+}
